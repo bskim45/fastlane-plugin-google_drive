430d6fb1be01f08ab1d569750987df464725141f
Bumsoo Kim <bskim45@gmail.com>
2019-10-21 00:43:55 +0900

    deprecation notice for aliases

--- lib/fastlane/plugin/google_drive/actions/upload_google_drive_action.rb
+++ lib/fastlane/plugin/google_drive/actions/upload_google_drive_action.rb
@@ -9,6 +9,16 @@ module Fastlane
       def self.description
         "Alias for the `upload_to_google_drive` action"
       end
+
+      def self.category
+        :deprecated
+      end
+
+      def self.deprecated_notes
+        [
+          "Please use `upload_to_google_drive` instead."
+        ].join("\n")
+      end
     end
   end
 end
